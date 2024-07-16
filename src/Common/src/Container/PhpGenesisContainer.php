f3b6bf3ec272b3e9097f9deff000faf0cc7dad86
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-07-16 11:53:32 -0500

    [Common] Fix Container Fallbacks

--- src/Common/src/Container/PhpGenesisContainer.php
+++ src/Common/src/Container/PhpGenesisContainer.php
@@ -12,17 +12,25 @@ use Illuminate\Foundation\Application;
 /** @experimental */
 class PhpGenesisContainer extends IlluminateContainer
 {
-    public function __construct()
+    public static function getInstance(): IlluminateContainer|PhpGenesisContainer
     {
-        if (!$this->isLaravel()) {
-            // Create a new instance of the Illuminate container
-            $container = new IlluminateContainer();
-            static::setInstance($container);
+        if (static::isLaravelApplication()) {
+            return parent::getInstance();
         }
 
+        if (is_null(static::$instance)) {
+            static::$instance = new static; //@phpstan-ignore-line
+        }
+
+        return static::$instance;
     }
 
     public function isLaravel(): bool
+    {
+        return static::isLaravelApplication();
+    }
+
+    protected static function isLaravelApplication(): bool
     {
         return class_exists(Application::class);
     }

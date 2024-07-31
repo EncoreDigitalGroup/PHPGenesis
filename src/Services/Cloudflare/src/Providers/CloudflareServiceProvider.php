88f472d055832f4c98d9528fab4395923f0e8231
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2024-07-31 13:49:24 -0500

    [Cloudflare] Add Cloudflare Service (#16)
    Co-authored-by: onairmarc <onairmarc@users.noreply.github.com>

---
+++ src/Services/Cloudflare/src/Providers/CloudflareServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Copyright (c) 2024. Encore Digital Group.
+ * All Right Reserved.
+ */
+
+namespace PHPGenesis\Services\Cloudflare\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use PHPGenesis\Services\Cloudflare\Console\Commands\UpdateCloudflareDomainContactsCommand;
+
+/** @experimental */
+class CloudflareServiceProvider extends ServiceProvider
+{
+    public function register(): void {}
+
+    public function boot(): void
+    {
+        $this->commands([
+            UpdateCloudflareDomainContactsCommand::class,
+        ]);
+    }
+}

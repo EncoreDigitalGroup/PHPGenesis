d7b0a0b502eabce4de2e0488a0dea3ac6e15fa0e
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-08-23 14:37:11 -0500

    [Common] Create ComposerServiceProvider

---
+++ src/Common/src/Laravel/Providers/ComposerServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * Copyright (c) 2024. Encore Digital Group.
+ * All Right Reserved.
+ */
+
+namespace PHPGenesis\Common\Laravel\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ComposerServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $composerJsonPath = base_path('composer.json');
+        $composerConfig = json_decode(file_get_contents($composerJsonPath), true);
+
+        if (isset($composerConfig['extra']['laravel']['providers'])) {
+            foreach ($composerConfig['extra']['laravel']['providers'] as $provider) {
+                $this->app->register($provider);
+            }
+        }
+
+        if (isset($composerConfig['extra']['laravel']['aliases'])) {
+            foreach ($composerConfig['extra']['laravel']['aliases'] as $alias => $class) {
+                $this->app->alias($class, $alias);
+            }
+        }
+    }
+
+    public function boot(): void
+    {
+    }
+}

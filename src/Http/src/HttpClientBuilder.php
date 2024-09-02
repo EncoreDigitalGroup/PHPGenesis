dcc802aae197145f2c1fe4d4b6305f68e49e7f1d
Marc Beinder <marc@marcbeinder.com>
2024-09-01 21:30:29 -0500

    [Http] Fix Http Client

--- src/Http/src/HttpClientBuilder.php
+++ src/Http/src/HttpClientBuilder.php
@@ -1,47 +1,28 @@
 <?php
-/*
- * Copyright (c) 2024. Encore Digital Group.
- * All Right Reserved.
- */
 
 namespace PHPGenesis\Http;
 
+use Illuminate\Support\Facades\Facade;
 use Illuminate\Http\Client\Factory as HttpFactory;
-use Illuminate\Support\Facades\Http as HttpFacade;
 use PHPGenesis\Common\Container\PhpGenesisContainer;
 
-/** @experimental */
 class HttpClientBuilder
 {
     protected PhpGenesisContainer $container;
 
     public function __construct()
     {
-        $this->container = PhpGenesisContainer::getInstance();
-        $this->setupHttpFacade();
-    }
+        if (!PhpGenesisContainer::isLaravel()) {
+            // Step 1: Initialize the service container
+            $this->container = new PhpGenesisContainer;
 
-    protected function setupHttpFacade(): HttpFacade|HttpFactory
-    {
-        if ($this->container->isLaravel()) {
-            // Use Laravel's existing Http facade
-            return HttpFacade::getFacadeRoot();
+            // Step 2: Bind the HttpFactory to the container
+            $this->container->singleton('http', function () {
+                return new HttpFactory();
+            });
+
+            // Step 3: Set the container as the facade application
+            Facade::setFacadeApplication($this->container);
         }
-
-        // Set up a new container and Http facade
-        $clientFactory = new HttpFactory();
-
-        // Bind the HTTP Factory to the container
-        $this->container->getInstance()->singleton('http', function () use ($clientFactory) {
-            return $clientFactory;
-        });
-
-        // Alias the Http facade to the HTTP Factory
-        HttpFacade::setFacadeApplication(null);
-        HttpFacade::resolved(function ($httpFactory): void {
-            HttpFacade::swap($httpFactory);
-        });
-
-        return $clientFactory;
     }
 }

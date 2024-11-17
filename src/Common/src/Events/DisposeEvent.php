f3b64ae83911ac503fb2a892fd217ed22148af06
Marc Beinder <marc@marcbeinder.com>
2024-11-16 19:06:56 -0600

    [Common] Create IDisposable

---
+++ src/Common/src/Events/DisposeEvent.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Copyright (c) 2024. Encore Digital Group.
+ * All Right Reserved.
+ */
+
+namespace PHPGenesis\Common\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+
+class DisposeEvent
+{
+    use Dispatchable;
+
+    public function __construct()
+    {
+    }
+}

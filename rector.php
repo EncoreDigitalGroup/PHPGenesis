fea08ffabd4d97a7bcf37f791075bc09f2cf7091
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2025-09-12 09:40:07 -0500

    [DevUtilities] Add Rector::configure() method to preload Encore Digital Group standard Rector Config (#155)

--- rector.php
+++ rector.php
@@ -2,16 +2,14 @@
 
 /*
  * Copyright (c) 2024-2025. Encore Digital Group.
- * All Rights Reserved.
+ * All Right Reserved.
  */
 
 declare(strict_types=1);
 
 use PHPGenesis\DevUtilities\Rector\Rector;
-use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()
+return Rector::configure()
     ->withPaths([
         __DIR__ . "/src",
-    ])
-    ->withRules(Rector::rules());
+    ]);

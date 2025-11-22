483b43008c70b0402026c42ad02a85372cc08161
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2025-11-22 11:40:25 -0600

    [Laravel] Add PHPStan extension for Laravel Eloquent #[Scope] attribute support (#189)
    Co-authored-by: EncoreBot <ghbot@encoredigitalgroup.com>

---
+++ src/Laravel/src/PHPStan/Rules/Eloquent/Scopes/EloquentScopeAttributeExtension.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * Copyright (c) 2025. Encore Digital Group.
+ * All Rights Reserved.
+ */
+
+declare(strict_types=1);
+
+namespace PHPGenesis\Laravel\PHPStan\Rules\Eloquent\Scopes;
+
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Model;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\MethodsClassReflectionExtension;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\ShouldNotHappenException;
+use ReflectionAttribute;
+use ReflectionMethod;
+
+/** @internal */
+readonly class EloquentScopeAttributeExtension implements MethodsClassReflectionExtension
+{
+    public function __construct(
+        private ReflectionProvider $reflectionProvider
+    ) {}
+
+    public function hasMethod(ClassReflection $classReflection, string $methodName): bool
+    {
+        // Only process Eloquent models
+        if (!$this->reflectionProvider->hasClass(Model::class)) {
+            return false;
+        }
+
+        $modelReflection = $this->reflectionProvider->getClass(Model::class);
+        if (!$classReflection->isSubclassOfClass($modelReflection) && $classReflection->getName() !== Model::class) {
+            return false;
+        }
+
+        return $this->findScopeMethod($classReflection, $methodName) instanceof ReflectionMethod;
+    }
+
+    public function getMethod(ClassReflection $classReflection, string $methodName): MethodReflection
+    {
+        $scopeMethod = $this->findScopeMethod($classReflection, $methodName);
+
+        if (!$scopeMethod instanceof ReflectionMethod) {
+            throw new ShouldNotHappenException(
+                "Scope method {$methodName} not found on {$classReflection->getName()}"
+            );
+        }
+
+        return new EloquentScopeMethodReflection(
+            $methodName,
+            $classReflection,
+            $scopeMethod
+        );
+    }
+
+    /** Find a method on the class that has the #[Scope] attribute. */
+    private function findScopeMethod(ClassReflection $classReflection, string $methodName): ?ReflectionMethod
+    {
+        $nativeReflection = $classReflection->getNativeReflection();
+
+        if (!$nativeReflection->hasMethod($methodName)) {
+            return null;
+        }
+
+        $method = $nativeReflection->getMethod($methodName);
+        $attributes = $method->getAttributes(Scope::class, ReflectionAttribute::IS_INSTANCEOF);
+
+        if (count($attributes) === 0) {
+            return null;
+        }
+
+        return $method;
+    }
+}
---
+++ src/Laravel/src/PHPStan/Rules/Eloquent/Scopes/EloquentScopeMethodReflection.php
@@ -0,0 +1,139 @@
+<?php
+
+/*
+ * Copyright (c) 2025. Encore Digital Group.
+ * All Rights Reserved.
+ */
+
+declare(strict_types=1);
+
+namespace PHPGenesis\Laravel\PHPStan\Rules\Eloquent\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use PHPStan\Reflection\ClassMemberReflection;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\FunctionVariant;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\ParameterReflection;
+use PHPStan\TrinaryLogic;
+use PHPStan\Type\Generic\GenericObjectType;
+use PHPStan\Type\Generic\TemplateTypeMap;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+use ReflectionMethod;
+use ReflectionParameter;
+
+/** @internal */
+readonly class EloquentScopeMethodReflection implements MethodReflection
+{
+    public function __construct(
+        private string $methodName,
+        private ClassReflection $classReflection,
+        private ReflectionMethod $scopeMethod
+    ) {}
+
+    public function getDeclaringClass(): ClassReflection
+    {
+        return $this->classReflection;
+    }
+
+    public function isStatic(): bool
+    {
+        return true;
+    }
+
+    public function isPrivate(): bool
+    {
+        return false;
+    }
+
+    public function isPublic(): bool
+    {
+        return true;
+    }
+
+    public function getDocComment(): ?string
+    {
+        return $this->scopeMethod->getDocComment() ?: null;
+    }
+
+    public function getName(): string
+    {
+        return $this->methodName;
+    }
+
+    public function getPrototype(): ClassMemberReflection
+    {
+        return $this;
+    }
+
+    public function getVariants(): array
+    {
+        $parameters = $this->getParametersFromScopeMethod();
+
+        // Return type is Builder<ModelClass>
+        $modelType = new ObjectType($this->classReflection->getName());
+        $returnType = new GenericObjectType(
+            Builder::class,
+            [$modelType]
+        );
+
+        return [
+            new FunctionVariant(
+                TemplateTypeMap::createEmpty(),
+                null,
+                $parameters,
+                false,
+                $returnType
+            ),
+        ];
+    }
+
+    public function isDeprecated(): TrinaryLogic
+    {
+        return TrinaryLogic::createNo();
+    }
+
+    public function getDeprecatedDescription(): ?string
+    {
+        return null;
+    }
+
+    public function isFinal(): TrinaryLogic
+    {
+        return TrinaryLogic::createNo();
+    }
+
+    public function isInternal(): TrinaryLogic
+    {
+        return TrinaryLogic::createNo();
+    }
+
+    public function getThrowType(): ?Type
+    {
+        return null;
+    }
+
+    public function hasSideEffects(): TrinaryLogic
+    {
+        return TrinaryLogic::createMaybe();
+    }
+
+    /**
+     * Extract parameters from the scope method, excluding the first parameter (Builder $query).
+     *
+     * @return list<ParameterReflection>
+     */
+    private function getParametersFromScopeMethod(): array
+    {
+        $parameters = $this->scopeMethod->getParameters();
+
+        // Remove the first parameter (Builder $query)
+        array_shift($parameters);
+
+        return array_values(array_map(
+            fn (ReflectionParameter $param): ParameterReflection => new EloquentScopeParameterReflection($param),
+            $parameters
+        ));
+    }
+}
